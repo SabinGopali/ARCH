52ae21a5b22a8b598b54c40d4b83296f9884c554
Sabin Gopali <sabingopali22@gmail.com>
2025-06-29 20:29:41 +0545

    updated

--- Archphaze/src/redux/store.js
+++ Archphaze/src/redux/store.js
@@ -1,5 +1,5 @@
 import { configureStore, combineReducers  } from '@reduxjs/toolkit';
-import userReducer from './user/userSlice';
+import userReducer from './user/userslice';
 import { persistReducer, persistStore } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
